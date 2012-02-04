59b3a8a384c7561907843d71d0a219a51aad1b89
Michal Jedynak <m.jedynak@gmail.com>
2012-02-04 15:01:45 +0100

    added pico container

--- src/main/groovy/pl/mjedynak/idea/plugins/GenerateEqualsHashCodeDeluxeAction.groovy
+++ src/main/groovy/pl/mjedynak/idea/plugins/GenerateEqualsHashCodeDeluxeAction.groovy
@@ -1,10 +1,22 @@
 package pl.mjedynak.idea.plugins
 
 import com.intellij.openapi.editor.actionSystem.EditorAction
+import org.picocontainer.MutablePicoContainer
+import org.picocontainer.defaults.DefaultPicoContainer
+import pl.mjedynak.idea.plugins.psi.PsiHelper
 
 class GenerateEqualsHashCodeDeluxeAction extends EditorAction {
 
-    private static GenerateEqualsHashCodeDeluxeActionHandler handler = new GenerateEqualsHashCodeDeluxeActionHandler()
+    private static MutablePicoContainer picoContainer = new DefaultPicoContainer()
+
+    private static GenerateEqualsHashCodeDeluxeActionHandler handler
+
+    static {
+        picoContainer.registerComponentImplementation(PsiHelper)
+        picoContainer.registerComponentImplementation(GenerateEqualsHashCodeDeluxeActionHandler)
+        handler = picoContainer.getComponentInstanceOfType(GenerateEqualsHashCodeDeluxeActionHandler)
+    }
+
 
     protected GenerateEqualsHashCodeDeluxeAction() {
         super(handler)
--- src/main/groovy/pl/mjedynak/idea/plugins/GenerateEqualsHashCodeDeluxeActionHandler.groovy
+++ src/main/groovy/pl/mjedynak/idea/plugins/GenerateEqualsHashCodeDeluxeActionHandler.groovy
@@ -1,13 +1,28 @@
 package pl.mjedynak.idea.plugins
 
 import com.intellij.openapi.actionSystem.DataContext
+import com.intellij.openapi.actionSystem.DataKeys
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.editor.actionSystem.EditorActionHandler
+import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiClass
+import pl.mjedynak.idea.plugins.psi.PsiHelper
 
 class GenerateEqualsHashCodeDeluxeActionHandler extends EditorActionHandler {
 
+    private PsiHelper psiHelper
+
+    GenerateEqualsHashCodeDeluxeActionHandler(PsiHelper psiHelper) {
+        this.psiHelper = psiHelper
+    }
+
+
+
     @Override
     void execute(Editor editor, DataContext dataContext) {
-        println "aa"
+        Project project = (Project) dataContext.getData(DataKeys.PROJECT.getName());
+        PsiClass psiClass = psiHelper.getPsiClassFromEditor(editor, project)
+        println psiClass.getAllFields()
+
     }
 }
