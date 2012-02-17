c39fee8f42be48c328a3457e8a9049e2ff5bbbf3
Michal Jedynak <m.jedynak@gmail.com>
2012-02-17 23:03:33 +0100

    added unit tests for action handler

--- src/test/groovy/pl/mjedynak/idea/plugins/action/GenerateEqualsHashCodeDeluxeActionHandlerTest.groovy
+++ src/test/groovy/pl/mjedynak/idea/plugins/action/GenerateEqualsHashCodeDeluxeActionHandlerTest.groovy
@@ -1,6 +1,7 @@
 package pl.mjedynak.idea.plugins.action
 
 import com.intellij.codeInsight.CodeInsightBundle
+import com.intellij.codeInsight.generation.ClassMember
 import com.intellij.codeInsight.generation.GenerateEqualsHelper
 import com.intellij.codeInsight.hint.HintManager
 import com.intellij.openapi.application.Application
@@ -10,6 +11,8 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.DialogWrapper
 import com.intellij.openapi.ui.Messages
 import com.intellij.psi.PsiClass
+import com.intellij.psi.PsiField
+import com.intellij.psi.PsiModifier
 import com.intellij.psi.impl.source.PsiMethodImpl
 import com.intellij.psi.search.GlobalSearchScope
 import com.intellij.psi.util.MethodSignature
@@ -17,6 +20,7 @@ import javax.swing.Icon
 import pl.mjedynak.idea.plugins.factory.GenerateEqualsHashCodeDeluxeWizardFactory
 import pl.mjedynak.idea.plugins.generator.EqualsGenerator
 import pl.mjedynak.idea.plugins.generator.HashCodeGenerator
+import pl.mjedynak.idea.plugins.wizard.GenerateEqualsHashCodeDeluxeWizard
 import spock.lang.Specification
 
 class GenerateEqualsHashCodeDeluxeActionHandlerTest extends Specification {
@@ -24,8 +28,7 @@ class GenerateEqualsHashCodeDeluxeActionHandlerTest extends Specification {
     private static final int OK_EXIT_CODE = DialogWrapper.OK_EXIT_CODE
     private static final int NOT_OK_EXIT_CODE = DialogWrapper.OK_EXIT_CODE + 1
 
-    GenerateEqualsHashCodeDeluxeActionHandler actionHandler =
-        new GenerateEqualsHashCodeDeluxeActionHandler(guavaHashCodeGenerator, guavaEqualsGenerator, methodChooser, factory)
+    GenerateEqualsHashCodeDeluxeActionHandler actionHandler
 
     HashCodeGenerator guavaHashCodeGenerator = Mock()
     EqualsGenerator guavaEqualsGenerator = Mock()
@@ -41,58 +44,109 @@ class GenerateEqualsHashCodeDeluxeActionHandlerTest extends Specification {
     PsiMethodImpl hashCodeMethod = Mock()
     Application application = Mock()
     HintManager hintManager = Mock()
+    GenerateEqualsHashCodeDeluxeWizard wizard = Mock()
+
+    ClassMember[] result
+
 
     def setup() {
+        actionHandler = new GenerateEqualsHashCodeDeluxeActionHandler(guavaHashCodeGenerator, guavaEqualsGenerator, methodChooser, factory)
+
         GenerateEqualsHelper.metaClass.'static'.getEqualsSignature = { Project project, GlobalSearchScope scope -> equalsMethodSignature }
         GenerateEqualsHelper.metaClass.'static'.getHashCodeSignature = { hashCodeMethodSignature }
-        GenerateEqualsHelper.metaClass.'static'.findMethod = {PsiClass psiClass, MethodSignature methodSignature ->
-            if (methodSignature == equalsMethodSignature) {
-                equalsMethod
-            } else {
-                hashCodeMethod
-            }
-        }
+        GenerateEqualsHelper.metaClass.'static'.findMethod = {PsiClass psiClass, MethodSignature methodSignature -> null}
         CodeInsightBundle.metaClass.'static'.message = {String key -> "anyString"}
         Messages.metaClass.'static'.getQuestionIcon = {Mock(Icon)}
         ApplicationManager.metaClass.'static'.getApplication = {application}
         HintManager.metaClass.'static'.getInstance = {hintManager}
-
+        factory.createWizard(project, psiClass, true, true) >> wizard
     }
 
     def "does not display wizard when methods exist and user decides not to delete them"() {
-        userClicksNo()
+        equalsAndHashCodeExist()
+        userClicksNoInDeleteDialog()
 
         when:
-        def members = actionHandler.chooseOriginalMembers(psiClass, project, editor)
+        result = actionHandler.chooseOriginalMembers(psiClass, project, editor)
 
         then:
-        members == null
+        methodsNotCreated()
         0 * factory._
     }
 
+
     def "does not display wizard when methods exist but deletion is not successful"() {
-        userClicksYes()
+        equalsAndHashCodeExist()
+        userClicksYesInDeleteDialog()
         deletionNotSuccessful()
 
         when:
-        def members = actionHandler.chooseOriginalMembers(psiClass, project, editor)
+        result = actionHandler.chooseOriginalMembers(psiClass, project, editor)
 
         then:
-        members == null
+        methodsNotCreated()
         0 * factory._
     }
 
     def "displays error message when class has only static fields"() {
-        userClicksYes()
+        equalsAndHashCodeExist()
+        userClicksYesInDeleteDialog()
         deletionSuccessful()
+        classHasOnlyStaticFields()
 
         when:
-        def members = actionHandler.chooseOriginalMembers(psiClass, project, editor)
+        result = actionHandler.chooseOriginalMembers(psiClass, project, editor)
 
         then:
-        members == null
+        methodsNotCreated()
         1 * hintManager.showErrorHint(editor, actionHandler.ONLY_STATIC_FIELDS_ERROR)
         0 * factory._
+    }
+
+    def "shows wizard but does not create methods because user clicks cancel"() {
+        classHasNoStaticField()
+        userClicksCancelInWizard()
+
+        when:
+        result = actionHandler.chooseOriginalMembers(psiClass, project, editor)
+
+        then:
+        methodsNotCreated()
+        1 * wizard.show()
+    }
+
+    def "creates methods"() {
+        classHasNoStaticField()
+        userClicksOkInWizard()
+
+        when:
+        result = actionHandler.chooseOriginalMembers(psiClass, project, editor)
+
+        then:
+        methodsCreated()
+        1 * wizard.show()
+    }
+
+    def userClicksOkInWizard() {
+        wizard.isOK() >> true
+    }
+
+    def userClicksCancelInWizard() {
+        wizard.isOK() >> false
+    }
+
+    def classHasNoStaticField() {
+        PsiField field = Mock()
+        field.hasModifierProperty(PsiModifier.STATIC) >> false
+        PsiField[] fields = [field]
+        psiClass.fields >> fields
+    }
+
+    def classHasOnlyStaticFields() {
+        PsiField field = Mock()
+        field.hasModifierProperty(PsiModifier.STATIC) >> true
+        PsiField[] fields = [field]
+        psiClass.fields >> fields
     }
 
     def deletionNotSuccessful() {
@@ -103,11 +157,30 @@ class GenerateEqualsHashCodeDeluxeActionHandlerTest extends Specification {
         application.runWriteAction(_ as DeleteExistingMethodsComputable) >> true
     }
 
-    def userClicksYes() {
+    def userClicksYesInDeleteDialog() {
         Messages.metaClass.'static'.showYesNoDialog = {Project project, String message, String title, Icon icon -> OK_EXIT_CODE}
     }
 
-    def userClicksNo() {
+    def userClicksNoInDeleteDialog() {
         Messages.metaClass.'static'.showYesNoDialog = {Project project, String message, String title, Icon icon -> NOT_OK_EXIT_CODE}
     }
+
+    def equalsAndHashCodeExist() {
+        GenerateEqualsHelper.metaClass.'static'.findMethod = {PsiClass psiClass, MethodSignature methodSignature ->
+            if (methodSignature == equalsMethodSignature) {
+                equalsMethod
+            } else {
+                hashCodeMethod
+            }
+        }
+    }
+
+    def boolean methodsNotCreated() {
+        result == null
+    }
+
+    def boolean methodsCreated() {
+        result != null
+    }
+
 }
